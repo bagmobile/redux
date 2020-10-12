2d7cd7cca53adba8fd15268ab0619d7c78e55104
BeloyAG <beloy.a.g@sberbank.ru>
2020-10-12 15:54:23 +0300

    Initial commit

---
+++ redux.js
@@ -0,0 +1,41 @@
+const redux = require('redux');
+
+const initialState = {
+    counter: 0
+}
+
+const reducer = (state = initialState, action) => {
+
+    if (action.type === `ADD`){
+        return {counter: state.counter + 1}
+    }
+    if (action.type === `SUB`){
+        return {counter: state.counter - 1}
+    }
+    if (action.type === `ADD_NUMBER`){
+        return {counter: state.counter + action.value}
+    }
+
+    return state;
+}
+
+const store = redux.createStore(reducer);
+
+store.subscribe(() => {
+    console.log(`-`, store.getState());
+})
+
+const addCounter = {
+    type: `ADD`
+}
+
+//console.log(store.getState());
+
+store.dispatch(addCounter);
+//console.log(store.getState());
+
+store.dispatch({type: `SUB`})
+//console.log(store.getState());
+
+store.dispatch({type: `ADD_NUMBER`, value: 5})
+//console.log(store.getState());
---
+++ src/redux/rootReducer.js
@@ -0,0 +1,5 @@
+const initialState = {count: 0};
+
+export default function rootReducer(state = initialState, action) {
+    return state;
+}
